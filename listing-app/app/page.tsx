47b23384c40d5593df1d614cc202250be20ae110
Murendeni Mudau <43264524+mudauml15@users.noreply.github.com>
2025-02-09 21:02:23 +0200

    created blue print components and admin route

---
+++ listing-app/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className=" flex h-screen  justify-center items-center">
+        <p className="font-bold text-3xl font-serif"> Welcome to Listing App </p>
+      </div>
+    </>
+  );
+}
